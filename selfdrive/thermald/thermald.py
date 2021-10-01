40fe14a12cfe04bb1d029755ade38b8a79e836d1
jc01rho <rkjnice@gmail.com>
2021-10-02 01:13:30 +0900

    working ...

--- selfdrive/thermald/thermald.py
+++ selfdrive/thermald/thermald.py
@@ -17,7 +17,7 @@ from common.params import Params, ParamKeyType
 from common.realtime import DT_TRML, sec_since_boot
 from common.dict_helpers import strip_deprecated_keys
 from selfdrive.controls.lib.alertmanager import set_offroad_alert
-from selfdrive.controls.lib.pid import LatPIController
+from selfdrive.controls.lib.pid import LatPIDController
 from selfdrive.hardware import EON, TICI, PC, HARDWARE
 from selfdrive.loggerd.config import get_available_percent
 from selfdrive.pandad import get_expected_signature
@@ -197,7 +197,7 @@ def thermald_thread():
   thermal_config = HARDWARE.get_thermal_config()
 
   # TODO: use PI controller for UNO
-  controller = LatPIController(k_p=0, k_i=2e-3, neg_limit=-80, pos_limit=0, rate=(1 / DT_TRML))
+  controller = LatPIDController(k_p=0, k_i=2e-3, neg_limit=-80, pos_limit=0, rate=(1 / DT_TRML))
 
   # Leave flag for loggerd to indicate device was left onroad
   if params.get_bool("IsOnroad"):
