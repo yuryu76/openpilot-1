38f898e2843b00340902990a026ba536fe89a578
jc01rho <rkjnice@gmail.com>
2021-10-02 02:00:41 +0900

    fixed

--- selfdrive/locationd/locationd.cc
+++ selfdrive/locationd/locationd.cc
@@ -201,7 +201,7 @@ void Localizer::handle_sensors(double current_time, const capnp::List<cereal::Se
 
     // sensor time and log time should be close
     if (std::abs(current_time - sensor_time) > 0.1) {
-      LOGE("Sensor reading ignored, sensor timestamp more than 100ms off from log time");
+      //LOGE("Sensor reading ignored, sensor timestamp more than 100ms off from log time");
       return;
     }
 
