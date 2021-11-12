333c2903a1fd027d75a5809f1f98712d0056a019
geniuth <74520084+geniuth@users.noreply.github.com>
2021-11-12 13:19:20 +0900

    Update interface.py
    Delete jc01rho code

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -198,25 +198,6 @@ class CarInterface(CarInterfaceBase):
       self.CS.adaptive_Cruise = False
       self.CS.enable_lkas = True
 
-
-    #Added by jc01rho inspired by JangPoo
-    if self.CS.main_on  and ret.cruiseState.enabled and ret.gearShifter == GearShifter.drive and ret.vEgo > 2 and not ret.brakePressed :
-      if ret.cruiseState.available and not ret.seatbeltUnlatched and not ret.espDisabled and self.flag_pcmEnable_able :
-
-        if self.flag_pcmEnable_initialSet == False :
-          self.initial_pcmEnable_counter = self.initial_pcmEnable_counter + 1
-          if self.initial_pcmEnable_counter > 750 :
-            events.add(EventName.pcmEnable)
-            self.flag_pcmEnable_initialSet = True
-            self.flag_pcmEnable_able = False
-            self.initial_pcmEnable_counter = 0
-        else :
-          events.add(EventName.pcmEnable)
-          self.flag_pcmEnable_able = False
-          # self.flag_pcmEnable_initialSet = True
-          # self.initial_pcmEnable_counter = 0
-    else  :
-      self.flag_pcmEnable_able = True
     ret.events = events.to_msg()
 
     # copy back carState packet to CS
