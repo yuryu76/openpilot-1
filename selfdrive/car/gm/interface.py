7644d3f6a201c6967af17ab655c5116efb3886e2
geniuth <74520084+geniuth@users.noreply.github.com>
2021-11-14 11:45:15 +0900

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -19,7 +19,7 @@ class CarInterface(CarInterfaceBase):
     v_current_kph = current_speed * CV.MS_TO_KPH
     
     gas_max_bp = [0., 30., 60., 90.]
-    gas_max_v = [0.7, 0.7, 0.7, 0.5]
+    gas_max_v = [0.5, 0.7, 0.7, 0.5]
 
     brake_max_bp = [0, 70., 130.]
     brake_max_v = [-6., -3., -2.1]
@@ -96,7 +96,7 @@ class CarInterface(CarInterfaceBase):
 
     # longitudinal
     ret.longitudinalTuning.kpBP = [0., 30.*CV.KPH_TO_MS, 60.*CV.KPH_TO_MS, 90.*CV.KPH_TO_MS]
-    ret.longitudinalTuning.kpV = [1.2, 1.1, 0.9, 0.43]
+    ret.longitudinalTuning.kpV = [1.15, 1.1, 0.9, 0.43]
     #ret.longitudinalTuning.kpBP = [0., 10.*CV.KPH_TO_MS, 20.*CV.KPH_TO_MS, 40.*CV.KPH_TO_MS, 70.*CV.KPH_TO_MS, 100.*CV.KPH_TO_MS, 130.*CV.KPH_TO_MS]
     #ret.longitudinalTuning.kpV = [1.3, 1.0, 0.9, 0.8, 0.6, 0.5, 0.4]
     
@@ -113,7 +113,7 @@ class CarInterface(CarInterfaceBase):
     ret.longitudinalActuatorDelayUpperBound = 0.3
     
     ret.startAccel = -0.4
-    ret.stopAccel = -2.5
+    ret.stopAccel = -2.0
     ret.startingAccelRate = 2.0
     ret.stoppingDecelRate = 4.0
     ret.vEgoStopping = 0.6
