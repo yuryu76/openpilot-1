3156ae96512ddaebcf128003643ca0efadaef733
whrho <whrho@sparrowfasoo.com>
2021-11-23 11:26:11 +0900

    Revert "comma #22990"
    This reverts commit 9436c6d0e9513e3ae9de63af131cf1ed31398483.

--- selfdrive/modeld/runners/runmodel.h
+++ selfdrive/modeld/runners/runmodel.h
@@ -1,7 +1,6 @@
 #pragma once
 class RunModel {
 public:
-  virtual ~RunModel() {}
   virtual void addRecurrent(float *state, int state_size) {}
   virtual void addDesire(float *state, int state_size) {}
   virtual void addTrafficConvention(float *state, int state_size) {}
