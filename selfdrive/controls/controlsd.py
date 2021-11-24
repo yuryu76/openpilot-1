1fb3481ee84a7273304241f0fbf81d015cb223a3
geniuth <74520084+geniuth@users.noreply.github.com>
2021-11-24 21:37:45 +0900

    Update controlsd.py
    reduce curve speed rate

--- selfdrive/controls/controlsd.py
+++ selfdrive/controls/controlsd.py
@@ -399,7 +399,7 @@ class Controls:
                 
         a_y_max = 2.975 - v_ego * 0.0375  # ~1.85 @ 75mph, ~2.6 @ 25mph
         v_curvature = np.sqrt(a_y_max / np.clip(np.abs(curv), 1e-4, None))
-        model_speed = np.mean(v_curvature) * 0.85
+        model_speed = np.mean(v_curvature) * 0.7
 
         if model_speed < v_ego:
           self.curve_speed_ms = float(max(model_speed, 32. * CV.KPH_TO_MS))
