c30330c5cb11fc8c3d288b06af5334402bd3f02d
geniuth <74520084+geniuth@users.noreply.github.com>
2021-12-04 09:39:13 +0900

    Update interface.py

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -115,15 +115,15 @@ class CarInterface(CarInterfaceBase):
     ret.longitudinalActuatorDelayLowerBound = 0.10
     ret.longitudinalActuatorDelayUpperBound = 0.15
     
-    ret.startAccel = -0.2
-    ret.stopAccel = -1.2
-    ret.startingAccelRate = 0.8
-    ret.stoppingDecelRate = 0.3
-    ret.vEgoStopping = 0.6
-    ret.vEgoStarting = 0.5
+    ret.startAccel = 0.8
+    ret.stopAccel = -2.0
+    ret.startingAccelRate = 4.8
+    ret.stoppingDecelRate = 2.4
+    ret.vEgoStopping = 0.3
+    ret.vEgoStarting = 0.3
     ret.stoppingControl = True
     
-    ret.steerLimitTimer = 1.5
+    ret.steerLimitTimer = 2.5
     ret.radarTimeStep = 0.0667  # GM radar runs at 15Hz instead of standard 20Hz
 
     return ret
