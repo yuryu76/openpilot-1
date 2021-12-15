13a60f019b4bf35d1f41bdf1e7dbd86db5c84056
whrho <whrho@sparrowfasoo.com>
2021-12-15 15:33:39 +0900

    actually, revert safety_gm.h

--- panda/board/safety/safety_gm.h
+++ panda/board/safety/safety_gm.h
@@ -68,30 +68,77 @@ const uint32_t GM_RT_INTERVAL = 250000;    // 250ms between real time checks
 const int GM_GAS_INTERCEPTOR_THRESHOLD = 458;  // (610 + 306.25) / 2ratio between offset and gain from dbc file
 #define GM_GET_INTERCEPTOR(msg) (((GET_BYTE((msg), 0) << 8) + GET_BYTE((msg), 1) + (GET_BYTE((msg), 2) << 8) + GET_BYTE((msg), 3)) / 2) // avg between 2 tracks
 
-const CanMsg GM_TX_MSGS[] = {{384, 0, 4}, {1033, 0, 7}, {1034, 0, 7}, {715, 0, 8}, {880, 0, 6}, {512, 0, 6}, {789, 0, 5}, {800, 0, 6},  // pt bus
-                             {161, 1, 7}, {774, 1, 8}, {776, 1, 7}, {784, 1, 2},   // obs bus
-                             {789, 2, 5},  // ch bus
+// Safety-relevant CAN messages for Bolt EV
+#define MSG_RX_STEER      0x184   // RX from PSCM, for steering torque and status
+#define MSG_RX_WHEEL      0x34A   // RX from EBCM, for wheel speed
+#define MSG_RX_BUTTON     0x1E1   // RX from BCM, for Cruise Buttons
+#define MSG_RX_BRAKE      0xF1    // RX from EBCM, for Brake Position
+#define MSG_RX_GAS        0x1A1   // RX from EBCM, for Pedal Position
+#define MSG_RX_PEDAL      0x201   // RX from Pedal Interceptor
+
+#define MSG_TX_LKA        0x180   // TX by OP, for LKA commands
+#define MSG_TX_ALIVE      0x409   // TX by OP, for ASCM Alive, To do : We need to check if this message is used for Bolt EV or not.
+#define MSG_TX_ASCM       0x40A   // TX by OP, for ASCM, To do : We need to check if this message is used for Bolt EV or not.
+#define MSG_TX_ACC        0x370   // TX by OP, for ACC Status, To do : We need to check if this message is used for Bolt EV or not.
+#define MSG_TX_PEDAL      0x200   // TX by OP, for Pedal Interceptor
+#define MSG_TX_BRAKE      0x315   // maybe tx BY OP, but Bolt cannot brakes,
+
+#define MSG_REGEN         0x189   // TX/RX for Regen Paddle
+
+
+
+
+//const CanMsg GM_TX_MSGS[] = {{384, 0, 4}, {1033, 0, 7}, {1034, 0, 7}, {715, 0, 8}, {880, 0, 6}, {512, 0, 6}, {789, 0, 5}, {800, 0, 6},  // pt bus
+//                             {161, 1, 7}, {774, 1, 8}, {776, 1, 7}, {784, 1, 2},   // obs bus
+//                             {789, 2, 5},  // ch bus
+//                             {0x104c006c, 3, 3}, {0x10400060, 3, 5}};  // gmlan
+//
+//// TODO: do checksum and counter checks. Add correct timestep, 0.1s for now.
+//AddrCheckStruct gm_addr_checks[] = {
+//  {.msg = {{388, 0, 8, .expected_timestep = 100000U}, { 0 }, { 0 }}},
+//  {.msg = {{842, 0, 5, .expected_timestep = 100000U}, { 0 }, { 0 }}},
+//  {.msg = {{481, 0, 7, .expected_timestep = 100000U}, { 0 }, { 0 }}},
+//  {.msg = {{241, 0, 6, .expected_timestep = 100000U}, { 0 }, { 0 }}},
+//  {.msg = {{452, 0, 8, .expected_timestep = 100000U}, { 0 }, { 0 }}},
+//
+//
+
+const CanMsg GM_TX_MSGS[] = {{MSG_TX_LKA, 0, 4}, {MSG_TX_ALIVE, 0, 7}, {MSG_TX_ASCM, 0, 7}, {MSG_TX_ACC, 0, 6}, {MSG_TX_PEDAL, 0, 6}, {MSG_REGEN, 0, 7}, // pt bus
                              {0x104c006c, 3, 3}, {0x10400060, 3, 5}};  // gmlan
 
 // TODO: do checksum and counter checks. Add correct timestep, 0.1s for now.
 AddrCheckStruct gm_addr_checks[] = {
-  {.msg = {{388, 0, 8, .expected_timestep = 100000U}, { 0 }, { 0 }}},
-  {.msg = {{842, 0, 5, .expected_timestep = 100000U}, { 0 }, { 0 }}},
-  {.msg = {{481, 0, 7, .expected_timestep = 100000U}, { 0 }, { 0 }}},
-  {.msg = {{241, 0, 6, .expected_timestep = 100000U}, { 0 }, { 0 }}},
-  {.msg = {{452, 0, 8, .expected_timestep = 100000U}, { 0 }, { 0 }}},
+  {.msg = {{MSG_RX_STEER, 0, 8, .expected_timestep = 100000U}, { 0 }, { 0 }}},
+  {.msg = {{MSG_RX_WHEEL, 0, 5, .expected_timestep = 100000U}, { 0 }, { 0 }}},
+  {.msg = {{MSG_RX_BUTTON, 0, 7, .expected_timestep = 100000U}, { 0 }, { 0 }}},
+  {.msg = {{MSG_RX_BRAKE, 0, 6, .expected_timestep = 100000U}, { 0 }, { 0 }}},
+  {.msg = {{MSG_RX_GAS, 0, 7, .expected_timestep = 100000U}, { 0 }, { 0 }}},
 };
 #define GM_RX_CHECK_LEN (sizeof(gm_addr_checks) / sizeof(gm_addr_checks[0]))
 addr_checks gm_rx_checks = {gm_addr_checks, GM_RX_CHECK_LEN};
+int cam_can_bus = -1;
+int bus_camera = -1;
+int bus_vehicle = -1;
+
 
 static int gm_rx_hook(CANPacket_t *to_push) {
 
   bool valid = addr_safety_check(to_push, &gm_rx_checks, NULL, NULL, NULL);
 
+  int addr = GET_ADDR(to_push);
+  int bus = GET_BUS(to_push);
+
+  if (bus == 1 && (addr == MSG_TX_LKA)) {
+    cam_can_bus = 1;
+  }
+  if (bus == 2 && (addr == MSG_TX_LKA)) {
+    cam_can_bus = 2;
+  }
+
   if (valid && (GET_BUS(to_push) == 0U)) {
     int addr = GET_ADDR(to_push);
 
-    if (addr == 388) {
+    if (addr == MSG_RX_STEER) {
       int torque_driver_new = ((GET_BYTE(to_push, 6) & 0x7U) << 8) | GET_BYTE(to_push, 7);
       torque_driver_new = to_signed(torque_driver_new, 11);
       // update array of samples
@@ -100,61 +147,79 @@ static int gm_rx_hook(CANPacket_t *to_push) {
 
     // sample speed, really only care if car is moving or not
     // rear left wheel speed
-    if (addr == 842) {
+    if (addr == MSG_RX_WHEEL) {
       vehicle_moving = GET_BYTE(to_push, 0) | GET_BYTE(to_push, 1);
     }
 
     // ACC steering wheel buttons
-    if (addr == 481) {
+    if (addr == MSG_RX_BUTTON) {
       int button = (GET_BYTE(to_push, 5) & 0x70U) >> 4;
       switch (button) {
         case 2:  // resume
         case 3:  // set
+        case 5:  // main on
           controls_allowed = 1;
           break;
-        case 6:  // cancel
-          if (!gas_interceptor_detected) {
-            // Need to be able to cancel CC for Pedal to work
-            //TODO: Investigate swapping controls
-            controls_allowed = 1;
-          }
-          break;
+//        case 6:  // cancel
+//          if (!gas_interceptor_detected) {
+//            // Need to be able to cancel CC for Pedal to work
+//            //TODO: Investigate swapping controls
+//            controls_allowed = 1;
+//          }
+//          break;
         default:
           break;  // any other button is irrelevant
       }
     }
 
     // speed > 0
-    if (addr == 241) {
+    if (addr == MSG_RX_BRAKE) {
       // Brake pedal's potentiometer returns near-zero reading
       // even when pedal is not pressed
       brake_pressed = GET_BYTE(to_push, 1) >= 10U;
     }
 
-    if (addr == 452) { //TODO: can we safely use v2?
-      gas_pressed = GET_BYTE(to_push, 5) != 0U;
+//    if (addr == 452) { //TODO: can we safely use v2?
+//      gas_pressed = GET_BYTE(to_push, 5) != 0U;
+//    }
+    // Gas Interceptor Check
+    if (addr == MSG_RX_PEDAL) {
+      gas_interceptor_detected = 1;
+      //int gas_interceptor = GM_GET_INTERCEPTOR(to_push);
+      //gas_pressed = gas_interceptor > HONDA_GAS_INTERCEPTOR_THRESHOLD;
+      //gas_interceptor_prev = gas_interceptor;
     }
 
-    // exit controls on regen paddle
-    if (addr == 189) {
-      bool regen = GET_BYTE(to_push, 0) & 0x20U;
-      if (regen) {
-        controls_allowed = 1;
-      }
+    if ((addr == MSG_RX_GAS) && (!gas_interceptor_detected)) {
+      gas_pressed = GET_BYTE(to_push, 6) != 0;
     }
+
+/////////////////슐러는 189에 대해 조사하고있으나 기존 사용하던 판다펌에선 0x189(393) 이 리젠 으로 지정되어있음. 그러나 기존 어차피 gm_rx_hook 에서 패들 검사 안함
+/////////////////향후에 차차님에게 확인 필요
+    // exit controls on regen paddle
+//    if (addr == 189) {
+//      bool regen = GET_BYTE(to_push, 0) & 0x20U;
+//      if (regen) {
+//        controls_allowed = 1;
+//      }
+//    }
 
     // Pedal Interceptor
-    if (addr == 513) {
-      gas_interceptor_detected = 1;
-      int gas_interceptor = GM_GET_INTERCEPTOR(to_push);
-      gas_pressed = gas_interceptor > GM_GAS_INTERCEPTOR_THRESHOLD;
-      gas_interceptor_prev = gas_interceptor;
-    }
+////////////상단에서 이미 확인함.
+//    if (addr == MSG_RX_PEDAL) {
+//      gas_interceptor_detected = 1;
+//      int gas_interceptor = GM_GET_INTERCEPTOR(to_push);
+//      gas_pressed = gas_interceptor > GM_GAS_INTERCEPTOR_THRESHOLD;
+//      gas_interceptor_prev = gas_interceptor;
+//    }
+
+
 
     // Check if ASCM or LKA camera are online
     // on powertrain bus.
     // 384 = ASCMLKASteeringCmd
     // 715 = ASCMGasRegenCmd
+
     generic_rx_checks(((addr == 384) || (addr == 715)));
   }
   return valid;
@@ -185,7 +250,7 @@ static int gm_tx_hook(CANPacket_t *to_send) {
   bool current_controls_allowed = controls_allowed; // && !pedal_pressed;
 
   // GAS: safety check (interceptor)
-  if (addr == 512) {
+  if (addr == MSG_TX_PEDAL) {
     if (!current_controls_allowed) {
       if (GET_BYTE(to_send, 0) || GET_BYTE(to_send, 1)) {
         tx = 0;
@@ -194,21 +259,21 @@ static int gm_tx_hook(CANPacket_t *to_send) {
   }
 
   // BRAKE: safety check
-  if (addr == 789) {
-    int brake = ((GET_BYTE(to_send, 0) & 0xFU) << 8) + GET_BYTE(to_send, 1);
-    brake = (0x1000 - brake) & 0xFFF;
-    if (!current_controls_allowed) {
-      if (brake != 0) {
-        tx = 0;
-      }
-    }
-    if (brake > GM_MAX_BRAKE) {
-      tx = 0;
-    }
-  }
+//  if (addr == 789) {
+//    int brake = ((GET_BYTE(to_send, 0) & 0xFU) << 8) + GET_BYTE(to_send, 1);
+//    brake = (0x1000 - brake) & 0xFFF;
+//    if (!current_controls_allowed) {
+//      if (brake != 0) {
+//        tx = 0;
+//      }
+//    }
+//    if (brake > GM_MAX_BRAKE) {
+//      tx = 0;
+//    }
+//  }
 
   // LKA STEER: safety check
-  if (addr == 384) {
+  if (addr == MSG_TX_LKA) {
     int desired_torque = ((GET_BYTE(to_send, 0) & 0x7U) << 8) + GET_BYTE(to_send, 1);
     uint32_t ts = microsecond_timer_get();
     bool violation = 0;
@@ -277,79 +342,118 @@ static int gm_tx_hook(CANPacket_t *to_send) {
 
 static int gm_fwd_hook(int bus_num, CANPacket_t *to_fwd) {
 
+//  int bus_fwd = -1;
+//
+//  if (gm_allow_fwd && !gm_block_fwd) {
+//    if (bus_num == 0) {
+//      // TODO: Catch message 388 and edit the HandsOffSWlDetectionStatus to 1 (0 is hands off)
+//      // Note: Blocking 388 causes error message when pressing LKAS button
+//      bus_fwd = gm_camera_bus;
+//    }
+//    else if (bus_num == gm_camera_bus) {
+//      int addr = GET_ADDR(to_fwd);
+//      // block stock lkas messages and stock acc messages (if OP is doing ACC)
+//      //TODO: Blocking stock camera ACC will need to be an option
+//      int is_lkas_msg = (addr == 384);
+//      int is_acc_msg = false;
+//      //int is_acc_msg = (addr == 0x343);
+//      int block_msg = is_lkas_msg || is_acc_msg;
+//      if (!block_msg) {
+//        bus_fwd = 0;
+//      }
+//    }
+//  }
+//  else {
+//    // Evaluate traffic to determine if forwarding should be enabled (only camera on bus 2)
+//    if (!gm_allow_fwd && !gm_block_fwd && bus_num == gm_camera_bus) {
+//      int addr = GET_ADDR(to_fwd);
+//      int len = GET_LEN(to_fwd);
+//
+//      if ((addr == 384 && len != 4) //chassis bus has 384 of different size
+//        || (addr == 1120) // F_LRR_Obj_Header from object bus
+//        || (addr == 784) // ASCMHeadlight from object bus
+//        || (addr == 309) // LHT_CameraObjConfirmation_FO from object bus
+//        || (addr == 192) // Unknown id only on chassis bus
+//      ) {
+//        gm_block_fwd = true;
+//      }
+//      else if (addr == 384 && len == 4) {
+//        gm_good_cam_cnt++;
+//      }
+//
+//      if (gm_good_cam_cnt > 10) {
+//        gm_allow_fwd = true;
+//      }
+//    }
+//  }
+//
+//  return bus_fwd;
   int bus_fwd = -1;
+  int addr = GET_ADDR(to_fwd);
 
-  if (gm_allow_fwd && !gm_block_fwd) {
-    if (bus_num == 0) {
-      // TODO: Catch message 388 and edit the HandsOffSWlDetectionStatus to 1 (0 is hands off)
-      // Note: Blocking 388 causes error message when pressing LKAS button
-      bus_fwd = gm_camera_bus;
-    }
-    else if (bus_num == gm_camera_bus) {
-      int addr = GET_ADDR(to_fwd);
-      // block stock lkas messages and stock acc messages (if OP is doing ACC)
-      //TODO: Blocking stock camera ACC will need to be an option
-      int is_lkas_msg = (addr == 384);
-      int is_acc_msg = false;
-      //int is_acc_msg = (addr == 0x343);
-      int block_msg = is_lkas_msg || is_acc_msg;
-      if (!block_msg) {
-        bus_fwd = 0;
-      }
-    }
-  }
-  else {
-    // Evaluate traffic to determine if forwarding should be enabled (only camera on bus 2)
-    if (!gm_allow_fwd && !gm_block_fwd && bus_num == gm_camera_bus) {
-      int addr = GET_ADDR(to_fwd);
-      int len = GET_LEN(to_fwd);
-
-      if ((addr == 384 && len != 4) //chassis bus has 384 of different size
-        || (addr == 1120) // F_LRR_Obj_Header from object bus
-        || (addr == 784) // ASCMHeadlight from object bus
-        || (addr == 309) // LHT_CameraObjConfirmation_FO from object bus
-        || (addr == 192) // Unknown id only on chassis bus
-      ) {
-        gm_block_fwd = true;
-      }
-      else if (addr == 384 && len == 4) {
-        gm_good_cam_cnt++;
-      }
-
-      if (gm_good_cam_cnt > 10) {
-        gm_allow_fwd = true;
-      } 
+  if (cam_can_bus == 1) {
+    bus_camera = 1;   //camera PT can bus for grey panda and custom made harness
+  } else {
+    if (cam_can_bus != -1){
+      bus_camera = 2;  //camera PT can bus for comma ai harness
     }
   }
 
+  if (bus_num == bus_vehicle) {
+    bus_fwd = bus_camera;       //Forward all messages from vehicle
+  }
+  if (bus_num == bus_camera) {
+    if (addr == MSG_TX_LKA) {
+      bus_fwd = -1;    //Block LKA message from camera
+    } else {
+    bus_fwd = bus_vehicle;       //Forward all messages except LAK message
+    }
+  }
   return bus_fwd;
 }
 
 
 static const addr_checks* gm_init(int16_t param) {
-  gm_safety_param = (int)param;
-  gm_good_cam_cnt = 0;
-  gm_allow_fwd = false;
-  gm_block_fwd = false;
-  gm_camera_bus = 2;
-  gm_has_relay = true;
-
-  if (car_harness_status == HARNESS_STATUS_NC) {
-    //puts("gm_init: No harness attached, assuming OBD or Giraffe\n");
-    //OBD harness and older pandas use bus 1 and no relay
-    gm_has_relay = false;
-    gm_camera_bus = 1;
-  }
-
-  controls_allowed = true;
+//  gm_safety_param = (int)param;
+//  gm_good_cam_cnt = 0;
+//  gm_allow_fwd = false;
+//  gm_block_fwd = false;
+//  gm_camera_bus = 2;
+//  gm_has_relay = true;
+//
+//  if (car_harness_status == HARNESS_STATUS_NC) {
+//    //puts("gm_init: No harness attached, assuming OBD or Giraffe\n");
+//    //OBD harness and older pandas use bus 1 and no relay
+//    gm_has_relay = false;
+//    gm_camera_bus = 1;
+//  }
+//
+//  controls_allowed = true;
+//  relay_malfunction_reset();
+//  return &gm_rx_checks;
+  UNUSED(param);
+  controls_allowed = false;
   relay_malfunction_reset();
+  gas_interceptor_detected = 0;
+  cam_can_bus = -1;
+  bus_camera = -1;
+  //bus_radar = 1;  // Radar can bus, Bolt EV doesn't need this can bus
+  bus_vehicle = 0; //vehicle PT can bus for comma ai harness
   return &gm_rx_checks;
+
 }
 
 const safety_hooks gm_hooks = {
+//  .init = gm_init,
+//  .rx = gm_rx_hook,
+//  .tx = gm_tx_hook,
+//  .tx_lin = nooutput_tx_lin_hook,
+//  .fwd = gm_fwd_hook,
   .init = gm_init,
   .rx = gm_rx_hook,
   .tx = gm_tx_hook,
   .tx_lin = nooutput_tx_lin_hook,
   .fwd = gm_fwd_hook,
+//  .addr_check = gm_addr_checks,
+//  .addr_check_len = sizeof(gm_addr_checks) / sizeof(gm_addr_checks[0]),
 };
