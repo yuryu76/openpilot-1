5b24ad90bd24b734fa4c1e64497f70843acbb0be
whrho <whrho@sparrowfasoo.com>
2021-12-21 16:54:18 +0900

    fixed lat engage conidtions

--- selfdrive/car/gm/interface.py
+++ selfdrive/car/gm/interface.py
@@ -193,13 +193,14 @@ class CarInterface(CarInterfaceBase):
             self.CS.enable_lkas = True
             break
       else :#lat engage
-        self.CS.adaptive_Cruise = False
-        self.CS.enable_lkas = True
-        # for b in ret.buttonEvents:
-        #   if not self.CS.adaptive_Cruise and (b.type == ButtonType.altButton3 and b.pressed) : #and self.CS.adaptive_Cruise
-        #     self.CS.adaptive_Cruise = False
-        #     self.CS.enable_lkas = False
-        #     break
+        # self.CS.adaptive_Cruise = False
+        # self.CS.enable_lkas = True
+        #
+        for b in ret.buttonEvents:
+          if not self.CS.adaptive_Cruise and (b.type == ButtonType.altButton3 and b.pressed) : #and self.CS.adaptive_Cruise
+            self.CS.adaptive_Cruise = False
+            self.CS.enable_lkas = False
+            break
 
     else :
       if self.CS.main_on: #wihtout pedal case
