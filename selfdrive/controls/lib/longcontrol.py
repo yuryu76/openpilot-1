1fe808d81a8033faa9616712a4f9e8ec67500a4d
geniuth <74520084+geniuth@users.noreply.github.com>
2021-12-31 01:18:15 +0900

    Update longcontrol.py
    for stop and go

--- selfdrive/controls/lib/longcontrol.py
+++ selfdrive/controls/lib/longcontrol.py
@@ -85,6 +85,9 @@ class LongControl():
     else:
       v_target_future = 0.0
       a_target = 0.0
+      
+    if a_target > 0.:
+      a_target *= interp(CS.vEgo, [0., 3.], [1.5, 1.])
 
     # TODO: This check is not complete and needs to be enforced by MPC
     a_target = clip(a_target, ACCEL_MIN_ISO, ACCEL_MAX_ISO)
